dc9363e018840502c9af9761c1d3e7cd28f51c06
hieuminhtranwork <hieuminhtran071102@gmail.com>
2025-06-23 11:19:31 +0700

    Add `AboutUsManagement` and `CustomerList` pages for staff panel.

--- quangvinh-store/src/App.jsx
+++ quangvinh-store/src/App.jsx
@@ -14,6 +14,10 @@ import ProductDetail from "./pages/Customer/ProductDetails.jsx";
 import Payment from "./pages/Customer/Payment.jsx";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import InstructionManagement from "./pages/Staff/InstructionManagement.jsx";
+import PoliciesManagement from "./pages/Staff/PoliciesManagement.jsx";
+import AboutUsManagement from "./pages/Staff/AboutUsManagement.jsx";
+import CustomerList from "./pages/Staff/CustomerList.jsx";
 
 
 
@@ -34,6 +38,22 @@ function App() {
                         <Route path="*" element={<NotFound />} />z
                         <Route path="/products/:id" element={<ProductDetail />} />
                         <Route path="/payment" element={<Payment />} />
+                        {/*<Route path="/instruction" element={<Instruction />} />*/}
+                        <Route path="/instruction-management" element={<InstructionManagement />} />
+                        {/*<Route path="/policies" element={<Policies />} />*/}
+                        <Route path="/policies-management" element={<PoliciesManagement />} />
+                        {/*<Route path="/about-us" element={<AboutUs />} />*/}
+                        <Route path="/store-management" element={<AboutUsManagement />} />
+                        <Route path="/products" element={<div>Products Page</div>} />
+                        <Route path="/categories" element={<div>Categories Page</div>} />
+                        <Route path="/customers" element={<CustomerList />} />
+                        <Route path="/orders" element={<div>Orders Page</div>} />
+                        <Route path="/feedbacks" element={<div>Feedbacks Page</div>} />
+                        <Route path="/statistics" element={<div>Statistics Page</div>} />
+                        <Route path="/staff" element={<div>Staff Page</div>} />
+                        <Route path="/campaign-management" element={<div>Campaign Management Page</div>} />
+                        <Route path="/settings" element={<div>Settings Page</div>} />
+                        <Route path="/logout" element={<div>Logout Page</div>} />
                     </Routes>
                 </main>
                 <Footer />
