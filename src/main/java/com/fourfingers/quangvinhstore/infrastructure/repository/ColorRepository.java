04c905f3337d6e5792f60e3d3ae20331c4b0bf52
llMeoUll <luutieulong2203@gmail.com>
2025-07-09 23:17:28 +0700

    Fix bug in create - update product

--- src/main/java/com/fourfingers/quangvinhstore/infrastructure/repository/ColorRepository.java
+++ src/main/java/com/fourfingers/quangvinhstore/infrastructure/repository/ColorRepository.java
@@ -4,5 +4,5 @@ import com.fourfingers.quangvinhstore.infrastructure.schema.ColorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ColorRepository extends JpaRepository<ColorEntity, Long> {
-
+    boolean existsByColorHex(String colorHex);
 }
