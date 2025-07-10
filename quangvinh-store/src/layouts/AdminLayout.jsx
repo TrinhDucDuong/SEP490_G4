f0524b3ad9f563cc408a0c2c4d69399b6dffc9ec
hieuminhtranwork <hieuminhtran071102@gmail.com>
2025-07-11 06:25:32 +0700

    update admin layout and App.jsx

--- quangvinh-store/src/layouts/AdminLayout.jsx
+++ quangvinh-store/src/layouts/AdminLayout.jsx
@@ -1,18 +1,18 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import HeaderForManager from "../components/layout/admin/HeaderForManager.jsx";
-import SidebarForStaff from "../components/layout/admin/SidebarForStaff.jsx";
-
+import SidebarForAdmin from '../components/layout/admin/SidebarForAdmin';
+import HeaderForManager from '../components/layout/admin/HeaderForManager';
 
 const AdminLayout = () => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="min-h-screen bg-gradient-to-br from-yellow-50 via-orange-50 to-blue-50">
             <HeaderForManager />
-            <div className="flex flex-grow">
-                <SidebarForStaff />
-                <main className="flex-grow p-4">
+            <SidebarForAdmin />
+            <main className="ml-64 pt-20"> {/* Tăng pt-16 thành pt-20 để tránh overlap */}
+                <div className="p-6">
                     <Outlet />
-                </main>
-            </div>
+                </div>
+            </main>
         </div>
     );
 };
