675bdf6e51a3df992da5a708155b606185f81e6b
llMeoUll <luutieulong2203@gmail.com>
2025-07-11 13:42:26 +0700

    Done get color for staff

--- src/main/java/com/fourfingers/quangvinhstore/adapter/rest/staff/ManageColorController.java
+++ src/main/java/com/fourfingers/quangvinhstore/adapter/rest/staff/ManageColorController.java
@@ -3,6 +3,8 @@ package com.fourfingers.quangvinhstore.adapter.rest.staff;
 import com.fourfingers.quangvinhstore.usecase.boundary.staff.ColorManagementInputBoundary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class ManageColorController {
     private final ColorManagementInputBoundary colorManagementInputBoundary;
+
+    @GetMapping
+    public ResponseEntity<?> getAllColors() {
+        return ResponseEntity.ok(colorManagementInputBoundary.findAll());
+    }
 }
