3504bf442c8f41ce044ae0c45af12b4e3dab538e
llMeoUll <luutieulong2203@gmail.com>
2025-08-09 10:17:35 +0700

    Fix size websocket message

--- src/main/java/com/fourfingers/quangvinhstore/infrastructure/config/websocket/WebSocketConfig.java
+++ src/main/java/com/fourfingers/quangvinhstore/infrastructure/config/websocket/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.fourfingers.quangvinhstore.infrastructure.config.websocket;
 
 import com.fourfingers.quangvinhstore.adapter.rest.admin.websocket.AiAssistanceWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -20,4 +22,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(aiAssistanceWebSocketHandler, "/admin/ai-assistant")
                 .setAllowedOrigins("*");
     }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(64 * 1024);
+        container.setMaxBinaryMessageBufferSize(64 * 1024);
+        return container;
+    }
+
 }
